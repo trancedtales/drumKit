c2eb71bcd990cc99729b5e99ba3ea09e79dd04dd
Aditya Kumar <adtya.031298@gmail.com>
2024-11-13 04:57:15 +0530

    final- 13-Nov

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ for (var i =0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click",function()  {
 
         var buttonInnerHTML = this.innerHTML;
+
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
 
     });
 }
@@ -17,6 +20,8 @@ for (var i =0; i<numberOfDrumButtons; i++) {
 //KEYBOARD PRESS
 document.addEventListener("keypress",function(event){
   makeSound(event.key);
+ 
+  buttonAnimation(event.key);
 });
 
 function makeSound (key) {
@@ -56,13 +61,23 @@ function makeSound (key) {
             kick.play();
             break;
 
-        default: console.log("no-sound")
+        default: console.log(buttonInnerHTML)
 
    
     }
+}
 
+function buttonAnimation(currentKey){
+    
+var activeButton = document.querySelector("." + currentKey);
 
+activeButton.classList.add("pressed");
 
-};
+setTimeout (function() {
 
+    activeButton.classList.remove("pressed");
+
+}, 100);
+
+}
 
